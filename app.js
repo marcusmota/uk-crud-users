8f321e179937ca452bdb643826d5107e626b337b
Marcus <marcushcsm@gmail.com>
2019-03-08 11:43:19 +0000

    add express validators configuration and fix error on listening twice the port on supertest

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const validator = require('express-validator');
+
 const app = express();
 const PORT = 3000;
 
+const userRoutes = require("./routes/User");
+
+app.use(validator())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
-})
+require('./config/db');
+
+userRoutes(app);
+
+if(!module.parent) {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+  })
+}
+
 
 module.exports = app;
