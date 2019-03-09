922e0c5d5f160de8eb0e6b1647eea469e821e480
Marcus <marcushcsm@gmail.com>
2019-03-09 14:49:59 +0000

    refactor on couldStoreEmailOnUpdate and create couldStoreEmailOnCreate on repository user integration test file

--- test/integration/repositories.User.js
+++ test/integration/repositories.User.js
@@ -64,9 +64,63 @@ describe('repositories/User Integration test', () => {
 
                 it('it should return false for a given user', async () => {
 
-                    const flag = await userRepository.couldStoreEmailOnUpdate("newemail@email.com");
+                    const data = {
+                        givenName : faker.name.firstName(),
+                        familyName : faker.name.lastName(),
+                        email : faker.internet.email(),
+                        created : faker.date.past()
+                    }
+            
+                    const user = await new userModel(data).save();
+
+                    const data2 = {
+                        givenName : faker.name.firstName(),
+                        familyName : faker.name.lastName(),
+                        email : faker.internet.email(),
+                        created : faker.date.past()
+                    }
+            
+                    const user2 = await new userModel(data2).save();
+
+                    const flag = await userRepository.couldStoreEmailOnUpdate(user2._id, user.email);
 
                     chai.expect(flag).to.eq(false);
+
+                });
+
+                it('it should return true for a given user', async () => {
+
+                    const flag = await userRepository.couldStoreEmailOnUpdate("5c81911bc86073004ed52f30", "newemail@email.com");
+
+                    chai.expect(flag).to.eq(true);
+
+                });
+            })
+
+            describe('couldStoreEmailOnCreate Methods', () => {
+
+                it('it should return false for a given user', async () => {
+
+                    const data = {
+                        givenName : faker.name.firstName(),
+                        familyName : faker.name.lastName(),
+                        email : faker.internet.email(),
+                        created : faker.date.past()
+                    }
+            
+                    await new userModel(data).save();
+
+                    const flag = await userRepository.couldStoreEmailOnCreate(data.email);
+
+                    chai.expect(flag).to.eq(false);
+
+                });
+
+                it('it should return true for a given user', async () => {
+
+                    const flag = await userRepository.couldStoreEmailOnCreate("newemail@email.com");
+
+                    chai.expect(flag).to.eq(true);
 
                 });
             })
