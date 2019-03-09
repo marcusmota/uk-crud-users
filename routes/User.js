a00c3e87269e9b21b3b8127f540ea7d6d14abb0f
Marcus <marcushcsm@gmail.com>
2019-03-09 14:44:52 +0000

    added the others crud routes

--- routes/User.js
+++ routes/User.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
     app.get('/v1/users', controller.getAll);
     app.get('/v1/user/:id', controller.getById);
     app.post('/v1/user', controller.postUser);
-    /* app.put('/v1/user/:id', controller.putUserById);
-    app.delete('/v1/user/:id', controller.deleteUserById); */
+    app.put('/v1/user/:id', controller.putUserById);
+    app.delete('/v1/user/:id', controller.deleteUserById);
   
 };
